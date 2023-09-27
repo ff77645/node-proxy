d905509eda3ae7a7659e58ed6d69df9e40c2b198
fy <fy77645@126.com>
2023-09-27 18:01:27 +0800

    代理优化

--- deeplx.js
+++ deeplx.js
@@ -8,9 +8,17 @@ import {getBody} from './util.js'
 
 let times = 0
 const COUNT = PROXYS.length
+let lastRequestTime = Date.now()
 
 const server = http.createServer(async function (req, res) {
-  const url = PROXYS[times++ % COUNT] + req.url
+  let proxy = PROXYS[0]
+  if(Date.now() - lastRequestTime < 1000){
+    proxy = PROXYS[times++ % COUNT]
+  }
+  req.pipe()
+  lastRequestTime = Date.now()
+  const url = proxy + req.url
+  console.log(`| ${resp.status} | ${times} | ${resp.statusText} | ${url} `);
   const body = await getBody(req)
   const resp = await fetch(url,{
     method:'post',
@@ -22,7 +30,6 @@ const server = http.createServer(async function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.statusCode = 200
   res.end(await resp.text())
-  console.log(`| ${resp.status} | ${resp.statusText} | ${url} `);
 })
 
 
